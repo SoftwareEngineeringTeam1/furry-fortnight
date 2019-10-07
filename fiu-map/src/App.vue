246ffe99d39269148d6f95e75a993f32b6e29066
Gianmarco Agostinone <42621516+gagostinone@users.noreply.github.com>
2019-10-07 15:31:29 -0400

    Started login page

--- fiu-map/src/App.vue
+++ fiu-map/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <!--
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
         <span>Vuetify</span>
@@ -14,20 +15,22 @@
         <span class="mr-2">Latest Release</span>
       </v-btn>
     </v-app-bar>
-
+    -->
     <v-content>
-      <HelloWorld />
+      <Login />
     </v-content>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld";
+import Login from "./views/Login";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    Login
   },
   data: () => ({
     //
---
+++ fiu-map/src/views/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card style="padding-top: 15px" align="center" justify="center" class="elevation-12">
+                <v-flex >
+              <v-img height="40%" width="40%" src="../assets/fiuPantherLogo.png"></v-img>
+                </v-flex>
+              <v-card-text>
+                <v-form>
+                  <v-text-field label="Login" name="login" filled type="text"></v-text-field>
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    filled
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn small text color="primary">New user</v-btn>
+                <v-btn small text color="primary">Forgot Password</v-btn>
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  })
+};
+</script>
