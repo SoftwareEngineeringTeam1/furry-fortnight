5e504e931bbfca44018de779af99b7616561eb34
rosamariab <rosamariabelisario@hotmail.com>
2019-10-31 14:17:04 -0400

    Front End + Databasecode

--- New folder/index.js
+++ New folder/index.js
@@ -1,26 +1,39 @@
-const express = require('express')
-const sql = require('mssql')
-const app = express()
-const port = 3000
-
-const pool = new sql.ConnectionPool
-({
-    user: 'ROSAMARIA-LENOV\\rosam',
+var mssql = require("mssql"); 
+var dbConfig = {
+    user: 'ROSAMARIA-LENOV\\rosam:1433',
     password: '',
-    server:'ROSAMARIA-LENOV\\RB',
+    server: 'ROSAMARIA-LENOV\\RB',
     database: 'furry'
-})
-;
+};
 
-pool.connect()
-    .then( success => {
-        console.log(success, 'we connected');
-        app.get('/', (req, res) => res.send('Hello World!'))
+var connection = mssql.connect(dbConfig, function (err) {
+    if (err)
+        throw err; 
+});
 
-        app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-    })
-    .catch( err => {
-        console.log(err);
+module.exports = connection; 
 
-    })
+// app.js 
+var db = require("db"); 
+var express = require("express"); 
+var app = express();
 
+app.get('/', function (req, res, next) {
+    const sql = require('mssql')
+ 
+    async () => {
+    try {
+        await sql.connect('mssql://ROSAMARIA-LENOV\\rosam@localhost:1433/fury')
+        const result = await sql.query`select * from category`
+        console.dir(result)
+        return result
+    } catch (err) {
+        console.log
+        // ... error checks
+    }
+}    
+}); 
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..'); 
+});
