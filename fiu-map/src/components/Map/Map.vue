8b0d07594a0e2b607b5431382a74525d5009f75c
Gianmarco Agostinone <42621516+gagostinone@users.noreply.github.com>
2019-11-04 18:09:41 -0500

    Updated Map.vue

--- fiu-map/src/components/Map/Map.vue
+++ fiu-map/src/components/Map/Map.vue
@@ -1,20 +1,39 @@
 <template>
   <div id="#app">
+  <a id="zz" v-on:click="handleClick('test')">Click me!</a>
+  <p id="zzz" v-show="1 === 1">hello</p>
+
     <MglMap id="map-container" 
         :accessToken="scene.accessToken" 
         :mapStyle.sync="scene.mapStyle"
         @load="onMapLoaded"
         :zoom="scene.zoom"
-        :pitch="pitch"
+        :pitch="scene.pitch"
         :center="scene.coordinates"
+        @zoomTo="scene.zoom"
     >
-      <MglMarker :coordinates="coordinates" color="blue" />
-      <MglNavigationControl position="top-right" />
-      <MglGeolocateControl position="top-right" />
-      <MglPopup :coordinates="popupCoordinates">
-        <span>Hello world!</span>
-      </MglPopup>
+     <div id="markd">
+      <MglMarker id="marker"
+        v-for="p in scene.coordinatesList" 
+        v-bind:coordinates="p.point"
+        v-bind:key="p.id"
+        v-bind:color="p.color"
+        v-bind:size="p.size"
+      >
+         <MglPopup >
+            <VCard>
+                <p>Hello, I'm popup! My coordinates are {{ p.point }}</p>
+            </VCard>
+        </MglPopup>
+      </MglMarker>
+      </div>
+    
+
+      <MglNavigationControl position="bottom-right" />
+      <MglGeolocateControl position="bottom-right" />
+
     </MglMap>
+
   </div>
 </template>
 
@@ -25,11 +44,11 @@ import {
     MglNavigationControl, 
     MglGeolocateControl, 
     MglPopup,
-    MglMarker 
+    MglMarker
 } from "vue-mapbox";
 
 export default {
-    name: 'App',
+    name: 'MapComponent',
     components: {
         MglMap,
         MglNavigationControl,
@@ -42,12 +61,27 @@ export default {
             scene: {
               accessToken: 'pk.eyJ1Ijoic3ctMjAxOSIsImEiOiJjazB1Mm1iNnQwanZ0M2NwZGdsNnpocmZkIn0.JODibKZFgAKA3cDqfbPraw',
               mapStyle: 'mapbox://styles/mapbox/streets-v11',
-              zoom: 15.3,
-              pitch: 0,
+              zoom: 15,
+              pitch: 10,
               navigationControl: true,
-              coordinates: [-80.37635, 25.756]
+              coordinates: [-80.37635, 25.756],
+              coordinatesList: [
+                {point: [-80.37615, 25.756], id: 1, color: '#aaaaaa', size: 'small'},
+                {point: [-80.40, 25.76], id: 2, color: '#11abcd', size: 'large'},
+                {point: [-80.38, 25.76], id: 3, color: '#cc9090', size: 'large'},
+                {point: [-80.37, 25.73], id: 4, color: '#234afd', size: 'large'},
+                {point: [-80.36, 25.72], id: 5, color: '#ee02d3', size: 'large'},
+                {point: [-80.35, 25.71], id: 6, color: '#f9f9f9', size: 'large'}
+              ]
             }
         }
+    },
+    const: {
+          coordinatesList2: [
+                {point: [-80.37615, 25.756], id: 1},
+                {point: [-80.38, 25.76], id: 2},
+                {point: [-80.6, 25.8], id: 3}
+              ]
     },
     props: {
       accessToken: {
@@ -69,17 +103,12 @@ export default {
       },
       zoom: {
         type: [Number, String],
-        default: 15.3
+        default: 3
       },
       pitch: {
         type: [Number, String],
-        default: 0
+        default: 50
       }
-    },
-    computed: {
-        popupCoordinates() {
-        return [10, 10]
-        }
     },
     created() {
         this.mapbox = Mapbox;
@@ -87,10 +116,10 @@ export default {
     methods: {
         onMapLoaded(event) {
             this.map = event.map;
-            this.$store.map = event.map;
-            
-            
-        }
+        },
+        handleClick: function(value) {
+            alert(value)
+    }
     }
 };
 </script>
@@ -104,5 +133,28 @@ export default {
     height: 100%;
     width: 100%;
     padding: 0px 0px;
+}
+#zz {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  z-index: 1;
+}
+#zzz {
+  position: absolute;
+  left: 30px;
+  top: 30px;
+  z-index: 1;
+}
+#mz {
+    color: red;
+}
+#marker:hover {
+    background-color: #000eee;
+    color: #000eee;
+}
+#markd {
+    width: 20px;
+    border-radius: 100%;
 }
 </style>
